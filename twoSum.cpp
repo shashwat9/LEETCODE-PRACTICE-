6b78a288e6c9e380ccad3babed4802a22c2c5894
Shashwat Raj <rajshashwat83401@gmail.com>
2022-06-30 18:15:03 +0530

    Add files via upload

---
+++ twoSum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<climits>
+#include<vector>
+#include<map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+     map<int,int>mp;
+        vector<int>ans;
+        for(int i = 0 ;i<nums.size();i++){
+            if(mp.find(target-nums[i]) != mp.end()){
+                ans.push_back(i);
+                ans.push_back(mp[target-nums[i]]);
+                break;
+            }
+            mp[nums[i]] = i;
+        }
+        return ans;
+}
